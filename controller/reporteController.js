b6884f4e0ea0350b97e5c457ab734e3c23817133
citux <cituxh@gmail.com>
2024-07-23 21:16:27 -0600

    Se agrega reporte rango fechas

--- controller/reporteController.js
+++ controller/reporteController.js
@@ -42,119 +42,36 @@ exports.getReporteVentasCliente = async (req, res) => {
   }
 };
 
-exports.getCatalogoVendedores = async (req, res) => {
-  try {
-    const vendedores = await sequelize.query(
-      `select id as value,cage, nage as label from grupo_sugua_data.dbo.ccage where activo = 1
-        `,
-      {
-        type: QueryTypes.SELECT,
-        raw: true,
-      }
-    );
-
-    res.status(200).send(vendedores);
-  } catch (e) {
-    console.log("error: ", e);
-    res.status(500).send("error en la creacion");
-  }
-};
-
-exports.getCatalogoTransporte = async (req, res) => {
+exports.gerReporteVentasxFecha = async (req, res) => {
+    let data = req.body;
     try {
-      const transporte = await sequelize.query(
-        `select id as value, ntransporte as label,  * from grupo_sugua_data.dbo.transportes
-          `,
+      const ventas = await sequelize.query(
+        `
+        EXEC	grupo_sugua_data.[dbo].[pibi_VentasxFechas] 
+		@dfecha1 = '${data.fecha_inicio}',
+		@dfecha2 = '${data.fecha_fin}', 
+		@ncli1 = 1, 
+		@ncli2 = 999999, 
+		@nlin1 = 1, 
+		@nlin2 = 99,
+		@cces1 = N'', 
+		@cces2 = N'ZZZZZZZZ',
+		@ncol1 = 0, 
+		@ncol2 = 999,
+		@npro1 = 1,
+		@npro2 = 999,
+		@nage1 = 1,
+		@nage2 = 999
+
+              `,
         {
           type: QueryTypes.SELECT,
           raw: true,
+          //plain: true,
         }
       );
   
-      res.status(200).send(transporte);
-    } catch (e) {
-      console.log("error: ", e);
-      res.status(500).send("error en la creacion");
-    }
-  };
-
-exports.setPedidos = async (req, res) => {
-    try {
-        let data = req.body
-
-        let max = await sequelize.query(
-            `SELECT ISNULL(MAX(noped),0) + 1 AS ultimo_pedido FROM grupo_sugua_data.dbo.portal_pedidosm
-              `,
-            {
-              type: QueryTypes.SELECT,
-              raw: true,
-              plain: true,
-            })
-
-        let master = {
-            empresa: data.empresa,
-            noped: max.ultimo_pedido,
-            fecha: data.fecha,
-            fecha_entrega: data.fecha_entrega,
-            estatus: 1,
-            codcli: data.codcli,
-            codven: data.codven,
-            nombre: data.nombre,
-            id_transporte: data.id_transporte,
-            direccion: data.direccion,
-            direccion_entrega: data.direccion_entrega,
-            operadopor: data.operadopor,
-            feoperado: data.feoperado,
-            total_venta: data.total_venta,
-            total_unidades: data.total_unidades,
-            observaciones: data.observaciones
-        }
-
-       let nuevoPedido = await sequelize.query(
-        `insert into grupo_sugua_data.dbo.portal_pedidosm 
-       (empresa,noped,fecha,fecha_entrega,estatus,codcli,codven,nombre,direccion,direccion_entrega,
-       operadopor,feoperado,total_venta,total_unidades,observaciones,id_transporte)
-        values(${master.empresa},${master.noped},'${master.fecha}','${master.fecha_entrega}',${master.estatus},
-        ${master.codcli},${master.codven},'${master.nombre}','${master.direccion}','${master.direccion_entrega}',
-        ${master.operadopor},'${master.feoperado}',${master.total_venta},${master.total_unidades},
-        '${master.observaciones}',${master.id_transporte})
-          `,
-        {
-          type: QueryTypes.INSERT,
-          returning: true,
-        })
-
-        for await (d of data.detalle) {
-    
-            let detalle = {
-                empresa: master.empresa,
-                id_pedido: max.ultimo_pedido,
-                id_prod: d.id_producto,
-                costo: d.costo,
-                cantidad: d.amount,
-                precio: d.price,
-                pordes: 0,
-                valdes: 0,
-                importe: d.amount * d.price,
-                descrip: d.brandName,
-                estatus: 1,
-                um: 1
-            }
-
-             let nuevoDetalle = await sequelize.query(
-                `insert into grupo_sugua_data.dbo.portal_pedidosd
-               (empresa,id_pedido,id_prod,costo,precio,cantidad,pordes,valdes,importe,descrip,estatus,um)
-                values(${detalle.empresa},${detalle.id_pedido},${detalle.id_prod},${detalle.costo},${detalle.precio},
-                ${detalle.cantidad},${detalle.pordes},${detalle.valdes},${detalle.importe},'${detalle.descrip}',${detalle.estatus},
-                ${detalle.um})
-                  `,
-                {
-                  type: QueryTypes.INSERT,
-                  returning: true,
-                })
-        }
-  
-      res.status(200).send({valid: true, msg: "pedido ingresado correctamente", data: {pedido: max.ultimo_pedido}});
+      res.status(200).send(ventas);
     } catch (e) {
       console.log("error: ", e);
       res.status(500).send("error en la creacion");
--- routes/reportes.js
+++ routes/reportes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const session = require('../session');
-const { gerReporteVentas, getReporteVentasCliente } = require("../controller/reporteController")
+const { gerReporteVentas, getReporteVentasCliente, gerReporteVentasxFecha } = require("../controller/reporteController")
 
 router.post('/ventas', session.check, gerReporteVentas);
+router.post('/ventasxfecha', session.check, gerReporteVentasxFecha);
+
 router.get('/clientes', session.check, getReporteVentasCliente);
 
 
