e4a453c1eec806359cf21c44ece1b1f7db33cff9
Luis Hidalgo <31080665+citux123@users.noreply.github.com>
2024-09-04 21:53:31 -0600

    conflicst
    commit conflicts

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const app = express();
 
 /* USES */
 app.use(bodyparser.json());
-app.use(cors());
+app.use(cors({
+ origin: '*', // Reemplaza con tu dominio frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',}));
 
 app.use(session.passport.initialize());
 
