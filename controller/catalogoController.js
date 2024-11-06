b9ceb6b3f25b436d79cf02c1381bfd0db53920d3
citux <cituxh@gmail.com>
2024-11-05 19:16:03 -0600

    Se agrega tallas a consulta

--- controller/catalogoController.js
+++ controller/catalogoController.js
@@ -67,36 +67,36 @@ exports.getProductos = async (req, res) => {
         productos = await sequelize.query(
           `
           SELECT 
-              RIGHT(CONVERT(VARCHAR(4),1000+productos.linea),3) + '  ' + lineas.nlinea AS linea_producto,   
-              RTRIM(LTRIM(productos.cestilo))  AS codigo_producto,  
-              RTRIM(LTRIM(productos.nestilo)) + ' ' + CASE WHEN productos_detalle.detalle = 'NO APLICA' THEN '' ELSE RTRIM(LTRIM(productos_detalle.detalle)) END AS descripcion, 
-              RIGHT(CONVERT(VARCHAR(4),1000+productos.ccolor),3) + '  ' + colores.ncolor AS color,   
-              SUM(ISNULL(stock.cantidad,0)) AS existencia,  
-              AVG(productos.precio1) AS precio,
-              productos.id AS id_producto,  
-              --productos.id AS codigo_producto,  
-              RIGHT(CONVERT(VARCHAR(4),1000+productos.ccolor),3) + '  ' + colores.ncolor AS seccion,   
-
-              AVG(productos.precio1) AS precio1, AVG(productos.precio2) AS precio2, 
-              AVG(productos.precio3) AS precio3, AVG(productos.precio4) AS precio4,   
-              AVG(productos.precio5) AS precio5, AVG(productos.precio6) AS precio6
-            FROM productos  
-              INNER JOIN lineas ON productos.empresa= lineas.empresa AND productos.linea = lineas.linea   
-              INNER JOIN colores ON productos.empresa= colores.empresa AND productos.ccolor = colores.ccolor   
-              LEFT  JOIN productos_detalle ON productos.detalle= productos_detalle.id 
-              LEFT  JOIN generos ON productos.empresa= generos.empresa AND productos.genero = generos.id 
-              LEFT  JOIN stock_pibi stock ON stock.periodo = (SELECT periodo FROM sysparameters WHERE id = ${empresa}) AND productos.empresa= stock.empresa AND productos.id = stock.id_prod 
-            WHERE 
-              productos.empresa = 4
-               ${precio ? "and productos.precio1 <= "+precio : "" }
+            RIGHT(CONVERT(VARCHAR(4),1000+productos.linea),3) + '  ' + lineas.nlinea AS linea_producto,   
+            RTRIM(LTRIM(productos.cestilo))  AS codigo_producto,  
+            RTRIM(LTRIM(productos.nestilo)) + ' ' + CASE WHEN productos_detalle.detalle = 'NO APLICA' THEN '' ELSE RTRIM(LTRIM(productos_detalle.detalle)) END AS descripcion, 
+            RIGHT(CONVERT(VARCHAR(4),1000+productos.ccolor),3) + '  ' + colores.ncolor AS color,   
+            SUM(ISNULL(stock.cantidad,0)) AS existencia,  
+            AVG(productos.precio1) AS precio,
+            productos.id AS id_producto,  productos.codrun,
+            RIGHT(CONVERT(VARCHAR(4),1000+productos.ccolor),3) + '  ' + colores.ncolor AS seccion,   
+            AVG(productos.precio1) AS precio1, AVG(productos.precio2) AS precio2, 
+            AVG(productos.precio3) AS precio3, AVG(productos.precio4) AS precio4,   
+            AVG(productos.precio5) AS precio5, AVG(productos.precio6) AS precio6,
+          runs.tallas_disponibles
+        FROM productos  
+            INNER JOIN lineas	ON productos.empresa= lineas.empresa AND productos.linea = lineas.linea   
+            INNER JOIN colores ON productos.empresa= colores.empresa AND productos.ccolor = colores.ccolor   
+            LEFT  JOIN productos_detalle ON productos.detalle= productos_detalle.id 
+            LEFT  JOIN generos ON productos.empresa= generos.empresa AND productos.genero = generos.id 
+            LEFT  JOIN stock_pibi stock	ON stock.periodo = (SELECT periodo FROM sysparameters WHERE id = 4) AND productos.empresa= stock.empresa AND productos.id = stock.id_prod 
+          LEFT  JOIN runs	ON productos.empresa = runs.empresa AND productos.codrun = runs.codrun
+          LEFT  JOIN runscfg ON runs.empresa = runscfg.empresa AND runs.run = runscfg.run
+        WHERE 
+            productos.empresa = 4 
+            ${precio ? "and productos.precio1 <= "+precio : "" }
             ${search ? "and productos.cestilo like '%" + search + "%'" + 
               " Or productos.nestilo like '%"+search+"%'"  : ""}
-            GROUP BY 
-              lineas.nlinea, productos.linea, productos.cestilo, productos.ccolor, productos.nestilo, productos_detalle.detalle, 
-              colores.ncolor, productos.id
-            ORDER BY 
-              productos.linea, productos.cestilo, productos.ccolor
-           
+        GROUP BY 
+            lineas.nlinea, productos.linea, productos.cestilo, productos.ccolor, productos.nestilo, productos_detalle.detalle, 
+            colores.ncolor, productos.id, productos.codrun, runs.tallas_disponibles
+        ORDER BY 
+            productos.linea, productos.cestilo, productos.ccolor
             OFFSET ${start} ROWS 
             FETCH NEXT ${limit} ROWS ONLY 
           `, {
@@ -318,7 +318,8 @@ exports.getProductoMatriz = async (req, res) => {
           ISNULL(runscfg.c07,'') AS c07, ISNULL(runscfg.c08,'') AS c08, ISNULL(runscfg.c09,'') AS c09, 
           ISNULL(runscfg.c10,'') AS c10, ISNULL(runscfg.c11,'') AS c11, ISNULL(runscfg.c12,'') AS c12, 
           ISNULL(runscfg.c13,'') AS c13, ISNULL(runscfg.c14,'') AS c14, ISNULL(runscfg.c15,'') AS c15, 
-          ISNULL(runscfg.c16,'') AS c16, ISNULL(runscfg.c17,'') AS c17, ISNULL(runscfg.c18,'') AS c18
+          ISNULL(runscfg.c16,'') AS c16, ISNULL(runscfg.c17,'') AS c17, ISNULL(runscfg.c18,'') AS c18,
+          runs.tallas_disponibles
         FROM productos 
           LEFT  JOIN productos_detalle ON productos.detalle= productos_detalle.id 
           INNER JOIN colores ON productos.empresa= colores.empresa AND productos.ccolor = colores.ccolor   
